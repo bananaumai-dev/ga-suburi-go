a6592f300edaf1d795deeca8eefc675447d5800d
YUTA SHIMAKAWA <banana.umai@gmail.com>
2020-04-28 14:53:00 +0900

    depends on go-bnn

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bananaumai-dev/ga-suburi-go
+
+go 1.13
+
+require github.com/bananaumai-dev/go-bnn v0.0.0-20200428053548-710d8d041d40
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bananaumai-dev/go-bnn/pkg/bnn"
+)
+
+func main() {
+	fmt.Printf("%s\n", bnn.Bnn())
+}
